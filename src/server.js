42121c3948d57e0cf63576204e35a35fe85b576f
Giovanni Toraldo <giovanni.toraldo@cloudesire.com>
2017-03-01 15:22:36 +0100

    semicolons

--- src/server.js
+++ src/server.js
@@ -1,12 +1,12 @@
 const http = require('http');
 const express = require('express');
-const cors = require('cors')
+const cors = require('cors');
 const Vibrant = require('node-vibrant');
 
 const app = express();
 const server = http.createServer(app);
 
-app.use(cors())
+app.use(cors());
 
 app.get('/', (req, res) => {
   const imgUrl = req.query.imgUrl;
