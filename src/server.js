f5187bfc29c06b64f01b742b3c1473f7df809028
Matteo Giordano <matteo.giordano@cloudesire.com>
2017-03-02 11:30:22 +0100

    Follow redirects

--- src/server.js
+++ src/server.js
@@ -2,6 +2,7 @@ const http = require('http');
 const express = require('express');
 const cors = require('cors');
 const Vibrant = require('node-vibrant');
+const request = require('request');
 
 const app = express();
 const server = http.createServer(app);
@@ -16,14 +17,25 @@ app.get('/', (req, res) => {
     return;
   }
 
-  Vibrant.from(imgUrl).getPalette((err, palette) => {
-    if (err != null) {
-      res.status(500).send(err.toString());
+  request({
+    url: imgUrl,
+    encoding: null
+  }, (error, response, body) => {
+    if (error != null) {
+      res.status(500).send(error);
+      return;
     }
 
-    const maxAge = process.env.MAX_AGE || 1296000;  // 15 days
-    res.set('Cache-Control', 'public, max-age=' + maxAge);
-    res.json(palette);
+    Vibrant.from(body).getPalette((err, palette) => {
+      if (err != null) {
+        res.status(500).send(err.toString());
+        return;
+      }
+
+      const maxAge = process.env.MAX_AGE || 1296000;  // 15 days
+      res.set('Cache-Control', 'public, max-age=' + maxAge);
+      res.json(palette);
+    });
   });
 });
 
--- test/test.js
+++ test/test.js
@@ -25,6 +25,18 @@ describe('loading express', () => {
       });
   });
 
+  it('follows redirects', (done) => {
+    request(server)
+      .get('/?imgUrl=https://backend.cloudesire.com/api/productImageFile/static/883.png')
+      .expect(200)
+      .end((err, res) => {
+        res.body.should.have.property('LightMuted');
+        res.body.LightMuted.should.have.property('rgb');
+        res.body.LightMuted.rgb.should.containDeep([153, 170, 171]);
+        done();
+      });
+  });
+
   it('responds to preflight cors requests', (done) => {
     request(server)
       .options('/?imgUrl=https://appshop.cloud/images/marketplace/logo.png')
@@ -50,5 +62,11 @@ describe('loading express', () => {
     request(server)
       .get('/foo/bar')
       .expect(404, done);
+  });
+
+  it('500 when not 200 or 300', (done) => {
+    request(server)
+      .get('/?imgUrl=https://httpbin.org/status/418')
+      .expect(500, done);
   });
 });
